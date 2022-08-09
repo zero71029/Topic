d7e1bd3079c9de58d80698fcfb99deb845371f45
zero71029 <wiz71029@gmail.com>
2022-08-09 10:06:55 +0800

    2022/08/09

--- src/main/java/com/jetec/topic/Tools/ZeroTools.java
+++ src/main/java/com/jetec/topic/Tools/ZeroTools.java
@@ -1,6 +1,10 @@
 package com.jetec.topic.Tools;
 
+import com.github.f4b6a3.uuid.UuidCreator;
+import com.jetec.topic.model.MemberBean;
 import org.json.JSONObject;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.io.BufferedReader;
@@ -18,66 +22,10 @@ import java.util.UUID;
 @Component
 public class ZeroTools {
 
-    // 筆記
-    ///////////////////////////////////////////
-//	返回上一頁
-//	window.history.back();location.reload();
-////////////////////
-// sql修改为不自动更新（只是插入是自动生成当前时间）
-//alter table hello change uptime uptime timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP	
-    //////////////////////////////////////
-//	重置 MySQL 自增列 AUTO_INCREMENT 初時值
-//	ALTER TABLE tbl AUTO_INCREMENT = 1;
-//////////////////////////////////
-//	一键复制至剪切板
-//	url.select();
-//    document.execCommand("Copy");
-//////jpa排序
-    // Sort sort = Sort.by(Direction.DESC,"marketid");
-    // return mr.findAll( sort);
-///////////////////////////////
-    //更新application
-//	HttpServletRequest sce
-//	ServletContext app = sce.getServletContext();
-//	app.setAttribute("billboardgroup", bgr.findAll());
-//////////ajax
-//	var formData = new FormData($(".uppdf")[0]);
-//	$.ajax({
-//        url: '${pageContext.request.contextPath}/CRM/selectContactByClientName/' + $("input[name='client']").val(),//接受請求的Servlet地址
-//        type: 'POST',
-//        // data: formData,
-//        // async: false,//同步請求
-//        // cache: false,//不快取頁面
-//        // contentType: false,//當form以multipart/form-data方式上傳檔案時，需要設定為false
-//        // processData: false,//如果要傳送Dom樹資訊或其他不需要轉換的資訊，請設定為false
-//        success: function (json) {
-//            $(".CCC").empty();
-//
-//            for (var j of json) {
-//                console.log(j);
-//
-//                $(".CCC").append('<div class="row TTT" onclick="clickContact(`' + j.name + '`,`' + j.phone + '`,`' + j.moblie + '`)">' +
-//                    '<div class="col-md-1"></div>' +
-//                    '<div class="col-md-3">' + j.name + '</div>' +
-//                    '<div class="col-md-3">' + j.phone + '</div>' +
-//                    '<div class="col-md-3">' + j.moblie + '</div>' +
-//                    '</div>');
-//            }
-//        },
-//        error: function (returndata) {
-//            console.log(returndata);
-//        }
-//    });
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//
-//    // 機器人判斷
+    // 機器人判斷
     public static boolean recaptcha(String toke) {
-
         String secret = "";
-
         // 取得token
-        System.out.println(toke);
         System.out.println("*****recaptcha******");
 
 //		JSONObject obj = new JSONObject();
@@ -128,20 +76,20 @@ public class ZeroTools {
      * @return String UUID
      */
     public static String getUUID() {
-        String s = UUID.randomUUID().toString();
+        String s = UuidCreator.getTimeOrderedWithRandom().toString();
         // 去掉“-”符號
         return s.substring(0, 8) + s.substring(9, 13) + s.substring(14, 18) + s.substring(19, 23) + s.substring(24);
     }
 
     /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//時間格式畫
+    //時間格式畫
     public static String getTime(Date date) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
         return sdf.format(date);
     }
 
     //////////////////////////////////////////////////////////////////////////////
-//    日期加幾天
+    //    日期加幾天
     public String addDay(String day, int i) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
         Calendar cal = Calendar.getInstance();
@@ -152,6 +100,20 @@ public class ZeroTools {
         }
         cal.add(Calendar.DATE, i);
         return dateFormat.format(cal.getTime());
+
+    }
+    //////////////////////////////////////////////////////////////////////////////
+    // 取得登入者
+    public static MemberBean getMemberBean(){
+        try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            return (MemberBean) authentication.getPrincipal();
+        }catch (Exception e){
+            MemberBean memberBean = new MemberBean();
+            memberBean.setName("未登入");
+            memberBean.setMemberid("");
+            return new MemberBean();
+        }
 
     }
 }
